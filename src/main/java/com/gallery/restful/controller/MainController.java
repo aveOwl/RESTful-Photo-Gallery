ceed19e7ef98381b38fc00cc620df276b8328120
Ave Owl <redowlave@gmail.com>
2016-07-02 20:02:17 +0300

    web template done

--- src/main/java/com/gallery/restful/controller/MainController.java
+++ src/main/java/com/gallery/restful/controller/MainController.java
@@ -3,6 +3,7 @@ package com.gallery.restful.controller;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Controller is used to manage all incoming requests.
@@ -10,12 +11,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @EnableAutoConfiguration
 public class MainController {
+
     /**
-     * Displaying home page.
+     * Redirecting from root path to front page.
+     * @return redirect link.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public final String redirecting() {
+        return "redirect:/photo";
+    }
+
+    /**
+     * Mapping for starting page.
      * @return view name.
      */
-    @RequestMapping(value = "/")
-    public final String start() {
-        return "index";
+    @RequestMapping(value = "/photo", method = RequestMethod.GET)
+    public final String frontPage() {
+        return "front-page";
+    }
+
+    /**
+     * Mapping for gallery page.
+     * @return view name.
+     */
+    @RequestMapping(value = "/photo/gallery", method = RequestMethod.POST)
+    public final String galleryPage() {
+        return "gallery-page";
     }
 }
